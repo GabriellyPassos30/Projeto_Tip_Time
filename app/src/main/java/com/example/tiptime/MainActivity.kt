eb875dffebc27af3658f706ccdfe752eabe277c1
GabriellyPassos30 <gabypassoss2@gmail.com>
2022-04-27 22:13:54 -0300

    [Projeto Kotlin]-Tip Time 3

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.example.tiptime
-
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import java.text.NumberFormat
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+
+    private fun calculateTip() {
+
+
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+
+
+        val cost = stringInTextField.toDouble()
+
+
+
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+
+        var tip = tipPercentage * cost
+
+
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+
+        val formatedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+
+        binding.tipResult.text = getString(R.string., formatedTip)
+    }
+
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
+
+
+
+
+
     }
 }
